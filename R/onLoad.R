648c22d3c48604bee57be566898106bfe266ab63
Noah Simon <nsimon@stanford.edu>
2010-10-12 00:00:00 +0000

    version 1.0

---
+++ R/onLoad.R
@@ -0,0 +1,4 @@
+.onLoad=function(libname,pkgname)
+{
+  packageStartupMessage("**********Loaded SGL**********","\n","Please note. Very few kittens were harmed in this implementation of the Sparse Group Lasso.","\n") 
+}
